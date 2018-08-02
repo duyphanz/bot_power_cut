9c6f902aee9446c722f4b8c39b93be52b18c4635
Duy Phan <phanduy344@gmail.com>
2018-08-02 14:19:24 +0700

    add server

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('done')
+})
+
+app.post('/message', (req, res) => {
+    console.log(req.body)
+    res.send('OK')
+})
+
+app.listen(process.env.PORT | 3000, (err) => {
+    if(err) return console.log('Error: ', err)
+    console.log('Server is running on port 3000.')
+})
